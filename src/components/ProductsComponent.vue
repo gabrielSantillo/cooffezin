4d2adb4445d01d6e22e8f5994ff79ce17f0743b7
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-02-15 14:34:16 -0700

    Added the elements of the products component

---
+++ src/components/ProductsComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="products">
+        <div class="products__bg section">
+            <div class="products__container container grid">
+                <div class="products__data">
+                    <a href="#" class="products__button">Scroll Down</a>
+
+                    <p class="products__description">
+                        We strive to form deep partnerships with farmers from all over the world to create perspective together and form helathy working relationships built on trust and respect.
+                    </p>
+                </div>
+
+                <div class="products__content">
+                    <article class="products__card">
+                        <img src="@/img/product-coffee-1.png" alt="products image" class="products__img">
+                        <h3 class="products__name">Classic Coffee</h3>
+                        <span class="products__price">$17.90</span>
+                    </article>
+
+                    <article class="products__card">
+                        <img src="@/img/product-coffee-2.png" alt="products image" class="products__img">
+                        <h3 class="products__name">Black Coffee</h3>
+                        <span class="products__price">$24.90</span>
+                    </article>
+
+                    <article class="products__card">
+                        <img src="@/img/product-coffee-3.png" alt="products image" class="products__img">
+                        <h3 class="products__name">Strong Coffee</h3>
+                        <span class="products__price">$32.90</span>
+                    </article>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '@/scss/variables.scss';
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="home">
     <home-component></home-component>
+    <products-component></products-component>
   </div>
 </template>
 
 <script>
 import HomeComponent from '@/components/HomeComponent.vue'
+import ProductsComponent from '@/components/ProductsComponent.vue'
 
 export default {
-  components: { HomeComponent },
+  components: { HomeComponent, ProductsComponent },
 
 }
 </script>
