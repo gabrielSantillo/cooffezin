fdb0861d6a5fa70970fac0937a4403b46f77fa7d
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2023-02-15 19:33:42 -0700

    Finish to style the testimonial component

--- src/components/TestimonialComponent.vue
+++ src/components/TestimonialComponent.vue
@@ -3,7 +3,7 @@
         <div class="testimonial__bg section">
             <div class="testimonial__container container grid">
                 <div class="testimonial__data">
-                    <h2 class="section__title">
+                    <h2 class="section__title testimonial__title">
                         Coffee is the best drink in the morning, it keeps you focused.
                     </h2>
 
@@ -13,6 +13,34 @@
                 </div>
 
                 <img src="@/img/testimonial-coffee.png" alt="testimonial image" class="testimonial__img">
+            </div>
+
+            <div class="testimonial__container container grid">
+                <div class="testimonial__data">
+                    <h2 class="section__title testimonial__title">
+                        Eating rice cakes is like chewing on a foam coffee cup, only less filling.
+                    </h2>
+
+                    <span class="testimonial__name">
+                        Dave Barry
+                    </span>
+                </div>
+
+                <img src="@/img/testimonial-coffee-2.jpeg" alt="testimonial image" class="testimonial__img">
+            </div>
+
+            <div class="testimonial__container container grid">
+                <div class="testimonial__data">
+                    <h2 class="section__title testimonial__title">
+                        I have measured out my life with coffee spoons.
+                    </h2>
+
+                    <span class="testimonial__name">
+                        T.S Elliot
+                    </span>
+                </div>
+
+                <img src="@/img/testimonial-coffee-3.jpeg" alt="testimonial image" class="testimonial__img">
             </div>
         </div>
     </div>
@@ -25,5 +53,31 @@
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/variables.scss";
+
+.testimonial__container {
+    row-gap: 3rem;
+    padding-top: 4rem;
+}
+
+.testimonial__data {
+    text-align: center;
+}
+
+.testimonial__title {
+    font-size: 2rem;
+}
+
+.testimonial__name {
+    font-weight: $--font-medium;
+}
+
+.testimonial__img {
+    width: 320px;
+    height: 240px;
+    object-fit: cover;
+    border-radius: 1rem;
+    justify-self: center;
+}
 
 </style>
